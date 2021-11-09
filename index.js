356e164a1e6e2718c7efbc6bfc3b0aeb05d865b2
unknown <tim.manankil@gmail.com>
2021-11-09 17:59:04 +0800

    added new js for slider

--- index.js
+++ index.js
@@ -2,11 +2,43 @@ const burger = document.querySelector('.burger');
 const navbar = document.querySelector('.navbar');
 
 
-const navSlide = () => {
-    burger.addEventListener('click', () => {
-        // console.log('burger burger!');
-        navbar.classList.toggle('navbar__active');
-    })
-}
+// const navSlide = () => {
+//     burger.addEventListener('click', () => {
+//         // console.log('burger burger!');
+//         navbar.classList.toggle('navbar__active');
+//     })
 
-navSlide();
+//     burger.addEventListener('keypress', (event) => {
+//         if( event.key == 'Enter' || event.key == ' ') {
+//             navbar.classList.toggle('navbar__active');
+//         }
+//     })
+
+//     console.log(event.key);
+// }
+
+// navSlide();
+
+const navSlider = () => {
+    
+    burger.addEventListener( 'click' , () => {
+        const slideVisible = navbar.getAttribute('nav-slide-visible');
+        
+
+        if(slideVisible === 'false'){
+            navbar.setAttribute('nav-slide-visible', true);
+            burger.setAttribute('aria-expanded', true);
+        }
+
+        else{
+            navbar.setAttribute('nav-slide-visible', false);
+            burger.setAttribute('aria-expanded', false);
+        }
+
+        // console.log(slideVisible);
+        // console.log(burger.getAttribute('aria-expanded'));
+    });
+
+};
+
+navSlider();
